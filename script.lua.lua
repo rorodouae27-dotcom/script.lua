31536d862f736c232ef9ee8b54b4afd8335b1ecb
rorodouae27-dotcom <rorodouae27@gmail.com>
2026-03-14 18:37:40 +0100

    Update script.lua.lua

--- script.lua.lua
+++ script.lua.lua
@@ -1916,7 +1916,7 @@ local leftWaypoints = {
     Vector3.new(-472.49, -7.00, 90.62),
     Vector3.new(-485.95, -4.64, 96.34),
     Vector3.new(-475.08, -7.00, 93.29),
-    Vector3.new(-474.22, -6.96, 16.18),
+    Vector3.new(-481.18, -5.04, 26.79),
 }
 
 local patrolMode         = "none"
